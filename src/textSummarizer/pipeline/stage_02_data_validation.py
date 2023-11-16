c130f9c585433013641be2779a389c211c8679f0
Boutaina ELYAZIJI <boutaina.mumu11@gmail.com>
2023-11-16 20:07:52 +0100

    data validation pipline added

--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -3,17 +3,13 @@ from textSummarizer.components.data_validation import DataValidation
 from textSummarizer.logging import logger
 
 
-STAGE_NAME="Data Validation"
-
 class DataValidationTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self) :
-        try:
             config = ConfigurationManager()
             data_validation_config = config.get_data_validation_config()
             data_validation = DataValidation(config=data_validation_config)
             data_validation.validate_all_files_exist()
-        except Exception as e:
-            raise e
+ 
