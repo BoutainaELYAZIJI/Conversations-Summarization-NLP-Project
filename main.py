36625b19ffc2177c20e31b02e66e770b3f2ce2e6
Boutaina ELYAZIJI <boutaina.mumu11@gmail.com>
2023-11-17 20:34:01 +0100

    data transformation pipeline added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transfomation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME="Data Validation "
@@ -21,6 +22,19 @@ STAGE_NAME="Data Validation "
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         data_validation = DataValidationTrainingPipeline()
+        data_validation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME="Data Transformation "
+
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_validation = DataTransformationTrainingPipeline()
         data_validation.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
